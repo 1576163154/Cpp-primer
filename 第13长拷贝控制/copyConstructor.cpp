1e48d13b078a8b6db4abf5c311864601a6abba3c
Xiang <849483053@qq.com>
2018-01-08 16:33:10 +0800

    。

--- 第13长拷贝控制/copyConstructor.cpp
+++ 第13长拷贝控制/copyConstructor.cpp
@@ -16,14 +16,22 @@ private:
 };
 
 
+void func(Sales_data sd) {
 
+}
+
+Sales_data func2(Sales_data& sd) {
+	return sd;
+}
 
 void main() {
 	Sales_data sd1("123",90);
 	Sales_data sd2 = sd1;//此时就会调用类的拷贝构造函数进行拷贝初始化
 	
+	func(sd1);//作为形参（非引用传入函数同样会
 
+	//这正好可以解释为何拷贝构造函数必须传入 引用类型，若传入形参则进入死循环
 
-
-	system("pause");
+	func2(sd2);//作为返回值时也会
+	
 }
