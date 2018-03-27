cb42812b9062e7d4fef2059df4446db21385d115
Xiang <849483053@qq.com>
2018-03-27 11:25:53 +0800

    lambda
    stable_sort <algorithm>

---
+++ 第14章重载运算与类型转换/14.1.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::stable_sort;
+//14.1 重载运算，要求左侧就是该类型
+         // 内置类型,要求左侧运算对象，是该类型 或 可以转换为该类型
+
+
+//14.8.1 lambda
+class ShorterString {
+public:
+	bool operator()(const string &s1, const string &s2)  const {
+		return s1.size() < s2.size();
+	}
+};
+
+
+void main() {
+	string s = "world";
+	string t = s + "!";
+	int i = 1;
+	double x = 2.0 + i;
+	
+
+	//lambda
+	vector<string>  vs = { "abc","ab","djfk" };
+	stable_sort(vs.begin(), vs.end(), ShorterString());
+
+	system("pause");
+}
